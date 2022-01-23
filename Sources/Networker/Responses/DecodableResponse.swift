36b2d5e49d2ba23930a85bce87a143f72669a4a8
Alexander Algashev <algashev@mail.ru>
2022-01-23 15:14:41 +0300

    Update DecodableResponse

--- Sources/Networker/Responses/DecodableResponse.swift
+++ Sources/Networker/Responses/DecodableResponse.swift
@@ -2,8 +2,14 @@ import Foundation
 
 /// The decoded message body (_or content_) data and
 /// the metadata associated with the response to an HTTP protocol URL load request.
+///
+/// More info see:
+/// [Encoding and Decoding Custom Types](https://developer.apple.com/documentation/foundation/archives_and_serialization/encoding_and_decoding_custom_types).
 public struct DecodableResponse<T: Decodable> {
     /// The decoded message body (_or content_) data.
+    ///
+    /// More info see:
+    /// [Encoding and Decoding Custom Types](https://developer.apple.com/documentation/foundation/archives_and_serialization/encoding_and_decoding_custom_types).
     public let decoded: T
     /// The metadata associated with the response to an HTTP protocol URL load request.
     public let response: HTTPURLResponse
