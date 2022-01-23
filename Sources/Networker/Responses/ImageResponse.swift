d92c9db19c788544bae245db25ac623ed934bec2
Alexander Algashev <algashev@mail.ru>
2022-01-23 15:05:21 +0300

    Add ImageResponse docs

--- Sources/Networker/Responses/ImageResponse.swift
+++ Sources/Networker/Responses/ImageResponse.swift
@@ -1,6 +1,12 @@
 import UIKit
 
+/// The decoded message body (_or content_) image data and
+/// the metadata associated with the response to an HTTP protocol URL load request.
 public struct ImageResponse {
+    /// The decoded message body (_or content_) image data.
     public let image: UIImage
+    /// The metadata associated with the response to an HTTP protocol URL load request.
     public let response: HTTPURLResponse
+    
+    
 }
