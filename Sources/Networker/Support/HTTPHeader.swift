2d94f789f001037a93a4dfb715587fb2778b6be2
Alexander Algashev <algashev@mail.ru>
2022-01-23 15:21:25 +0300

    Update HTTPHeader docs

--- Sources/Networker/Support/HTTPHeader.swift
+++ Sources/Networker/Support/HTTPHeader.swift
@@ -1,4 +1,16 @@
 /// A single name-value pair of HTTP header.
+///
+/// Example:
+/// ```swift
+/// let url = URL(string: "http://example.com/")!
+/// var urlRequest = URLRequest(url: url)
+/// urlRequest.setHTTPHeader(.contentType)
+/// let request = HTTPURLRequest(request: urlRequest)
+///
+/// extension HTTPHeader {
+///     static let contentType = HTTPHeader(name: "Content-Type", value: "text/html")
+/// }
+/// ```
 public struct HTTPHeader {
     /// The name of the header field. In keeping with the HTTP RFC, HTTP header field names are case insensitive.
     public var name: String
@@ -9,6 +21,18 @@ public struct HTTPHeader {
     /// - Parameters:
     ///   - name: The name of the header field. In keeping with the HTTP RFC, HTTP header field names are case insensitive.
     ///   - value: The value for the header field.
+    /// 
+    /// Example:
+    /// ```swift
+    /// let url = URL(string: "http://example.com/")!
+    /// var urlRequest = URLRequest(url: url)
+    /// urlRequest.setHTTPHeader(.contentType)
+    /// let request = HTTPURLRequest(request: urlRequest)
+    ///
+    /// extension HTTPHeader {
+    ///     static let contentType = HTTPHeader(name: "Content-Type", value: "text/html")
+    /// }
+    /// ```
     public init(name: String, value: String? = nil) {
         self.name = name
         self.value = value
